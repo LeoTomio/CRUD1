d2f4eb02192a7c4de84067a7021bd0657df39698
user <user@DESKTOP-F4RGEK8>
2021-02-18 17:35:57 -0300

    Mudanças

--- Modelo/Pessoa.php
+++ Modelo/Pessoa.php
@@ -2,10 +2,8 @@
 
 require 'Conexao.php';
 
-class Pessoa extends Conexao{
-    
-    
-    
+class Pessoa extends Conexao {
+
     public function inserir($nome, $idade, $cpf, $telefone, $sexo, $endereco) {
 
         $sql = "insert into usuario (nome, idade, cpf, telefone, sexo, endereco) values (?, ?, ?, ?, ?, ?);";
@@ -19,25 +17,10 @@ class Pessoa extends Conexao{
 
         $c->execute();
     }
-    
-        
-    public function alterar($codigo) {
 
-        $sql = "UPDATE usuario SET nome=?, idade=?, cpf=?, telefone=?, sexo=?, endereco=?) where $codigo =?);";
-        $c = self::conexao()->prepare($sql);
-        $c->bindValue(1, $nome);
-        $c->bindValue(2, $idade);
-        $c->bindValue(3, $cpf);
-        $c->bindValue(4, $telefone);
-        $c->bindValue(5, $sexo);
-        $c->bindValue(6, $endereco);
+    public function alterar($nome, $idade, $cpf, $telefone, $sexo, $endereco) {
 
-        $c->execute();
-    }
-   
-    public function deletar($codigo) {
-
-        $sql = "UPDATE usuario (nome, idade, cpf, telefone, sexo, endereco) where $codigo =?);";
+        $sql = "UPDATE usuario SET nome=?, idade=?, cpf=?, telefone=?, sexo=?, endereco=? where $codigo =?;";
         $c = self::conexao()->prepare($sql);
         $c->bindValue(1, $nome);
         $c->bindValue(2, $idade);
@@ -49,20 +32,22 @@ class Pessoa extends Conexao{
 
         $c->execute();
     }
-    
-       
+
+    public function deletar($codigo) {
+
+        $sql = "DELETE from usuario where $codigo =?;";
+        $c = self::conexao()->prepare($sql);
+        $c->bindValue(1, $codigo);
+        $c->execute();
+    }
+
     public function ler() {
 
         $sql = "Select * from usuario :";
         $c = self::conexao()->prepare($sql);
-        $c->bindValue(1, $nome);
-        $c->bindValue(2, $idade);
-        $c->bindValue(3, $cpf);
-        $c->bindValue(4, $telefone);
-        $c->bindValue(5, $sexo);
-        $c->bindValue(6, $endereco);
+        return $c;
 
         $c->execute();
     }
-    
+
 }
